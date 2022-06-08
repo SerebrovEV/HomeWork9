56daf33a82cbc0ce7a2614f5b19404609a68f441
Eugene <89818121788@mail.ru>
2022-06-09 00:03:19 +0300

    HW done

---
+++ src/pro/sky/java/course8/homework9/App.java
@@ -0,0 +1,14 @@
+package pro.sky.java.course8.homework9;
+
+public class App {
+    public static void main(String[] args) {
+        Author firstAuthor = new Author("Lev", "Tolstoy");
+        Author secondAuthor = new Author("Dmitriy", "Glushovskiy");
+        Book firstBook = new Book ("War and piece", firstAuthor, 1865);
+        Book secondBook = new Book("Metro 2033", secondAuthor, 2005);
+        System.out.println(firstBook.getNameBook() + ". " + firstBook.getAuthorName(firstBook) + ". " + firstBook.getYearOfPublication());
+        System.out.println(secondBook.getNameBook() + ". " + secondBook.getAuthorName(secondBook) + ". " + secondBook.getYearOfPublication());
+        firstBook.setYearOfPublication(2022);
+        System.out.println(firstBook.getYearOfPublication());
+    }
+    }
---
+++ src/pro/sky/java/course8/homework9/Author.java
@@ -0,0 +1,19 @@
+package pro.sky.java.course8.homework9;
+
+public class Author {
+
+    String nameAuthor;
+    String surnameAuthor;
+    public Author(String nameAuthor, String surnameAuthor) {
+        this.nameAuthor = nameAuthor;
+        this.surnameAuthor = surnameAuthor;
+    }
+
+    public String getNameAuthor() {
+        return nameAuthor;
+    }
+
+    public String getSurnameAuthor() {
+        return surnameAuthor;
+    }
+}
